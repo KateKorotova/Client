5ac9fa8228e958dfcb705c788e7b43e8872696bc
katya_000 <katya_000@KATEKOROTOVA>
2017-11-07 20:53:52 +0200

    change text

--- client_/Client.cpp
+++ client_/Client.cpp
@@ -2,6 +2,9 @@
 #include "Winsock2.h"
 #include <ws2tcpip.h>
 #include <string> 
+#include <vector>
+#include <fstream>
+#include <algorithm>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -41,10 +44,47 @@ int main() {
 	}
 
 	// Do while loop to send and receive data 
+	vector <string>origtxt;
+	ifstream file("text.txt");
+	while (!(file.eof())) {
+		int i = 0;
+		string tmp;
+		getline(file, tmp);
+		origtxt.push_back(tmp);
+		i++;
+	}
+	file.close();
+
 	char buf[4096];
 	string userInput;
 	do {
+		getline(cin, userInput);
+		if (userInput == "correct") {
+			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+			if (sendResult != SOCKET_ERROR) {
+				ZeroMemory(buf, 4096);
+				int bytesReceived = recv(sock, buf, 4096, 0);
+				string mess = string(buf);
+				if (mess != "No changes") {
+					int num = stoi(mess.substr(0, mess.find(",")));
+					mess.erase(mess.begin(), mess.begin() + mess.find(",") + 1);
+					string chs = mess;
+					if (num < origtxt.size())
+						origtxt[num] = chs;
+					else
+						origtxt.push_back(chs);
 
+					ofstream nfile("text.txt");
+					for (int i = 0; i < origtxt.size(); i++)
+						nfile << origtxt[i] >> endl; 
+					nfile.close();
+				}
+				cout << string(buf) << endl;
+
+			}
+		}
+			else
+				cout << "You can correct file" << endl;
 
 	} while (userInput.size() > 0);
 
